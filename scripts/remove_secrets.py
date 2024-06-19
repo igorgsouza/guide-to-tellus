8d3c332bbebfe10536a057edbe6a68f93f14985d
Igor Souza <igor.gs@hotmail.com>
2024-06-19 09:57:51 -0300

    Quartz sync: Jun 19, 2024, 9:57 AM

--- scripts/remove_secrets.py
+++ scripts/remove_secrets.py
@@ -2,9 +2,9 @@ import os
 import re
 
 def replace_in_md_files():
-    secret_block = re.compile(r'your_regex_pattern_here')
-    secret_line = re.compile(r'your_regex_pattern_here')
-    secret_link = re.compile(r'your_regex_pattern_here')
+    secret_block = re.compile(r'/```secret.*```/gmsU')
+    secret_line = re.compile(r'/`.*`/gmsU')
+    secret_link = re.compile(r'/\[\[_.*\]\]/gmU')
 
     for root, _, files in os.walk("./content"):
         for file in files:
@@ -18,7 +18,7 @@ def replace_in_md_files():
                 with open(file_path, "r", encoding="utf-8") as f:
                     content = f.read()
 
-                new_content = secret_line.sub("", secret_block.sub("", content))
+                new_content = secret_link.sub("[[SEGREDO]]", secret_line.sub("", secret_block.sub("", content)))
 
                 with open(file_path, "w", encoding="utf-8") as f:
                     f.write(new_content)
